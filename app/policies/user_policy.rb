28fd93309d699ee8f44b3e31a7b02cb475015794
Chris Hutchinson <hutchinson.252@osu.edu>
2018-02-02 23:31:56 -0500

    Fixing some indenting

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,25 +1,24 @@
 class UserPolicy
-	attr_reader :current_user, :user
+  attr_reader :current_user, :user
 
-	def initialize(current_user, user)
-		@current_user = current_user
-		@user = user
-	end
+  def initialize(current_user, user)
+    @current_user = current_user
+    @user = user
+  end
 
-	def index?
-		@current_user.admin?
-	end
+  def index?
+    @current_user.admin?
+  end
 
-	def show?
-		@current_user
-	end
+  def show?
+    @current_user
+  end
 
-	def edit?	
-		@current_user and (@current_user.admin? or @current_user == @user)
-	end	
-	
-	def update?	
-		puts "signed in: #{ @current_user }"
-		@current_user.admin? or @current_user == @user
-	end	
+  def edit?
+    @current_user and (@current_user.admin? or @current_user == @user)
+  end
+
+  def update?
+    @current_user.admin? or @current_user == @user
+  end	
 end

8e9d6dee121193e91c7d41266f295024c82a0212
Chris Hutchinson <hutchinson.252@osu.edu>
2018-02-10 22:23:04 -0500

    Doing more things

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,45 +5,48 @@ class Game < ApplicationRecord
 
   private
     def update_results
-      cover_team = nil
-      differential = self.away_team_score - self.home_team_score
+      if self.away_team_score and self.home_team_score
+       cover_team = nil
+       differential = self.away_team_score - self.home_team_score
 
-      if differential > 0
-	# If the home team was favored, but the away team won, then the away team covered.
-	if self.favored_team == self.home_team
-	  cover_team = self.away_team
-	else
-	  if differential.abs > self.spread.abs
+	if differential > 0
+	  # If the home team was favored, but the away team won, then the away team covered.
+	  if self.favored_team == self.home_team
 	    cover_team = self.away_team
 	  else
-	    cover_team = self.home_team
+	    if differential.abs > self.spread.abs
+	      cover_team = self.away_team
+	    else
+	      cover_team = self.home_team
+	    end
 	  end
-	end
-      else
-	# If the away team was favored, but the home team won, then the home team covered.	
-	if self.favored_team == self.away_team
-	  cover_team = self.home_team
 	else
-	  if differential.abs > self.spread.abs
+	  # If the away team was favored, but the home team won, then the home team covered.	
+	  if self.favored_team == self.away_team
 	    cover_team = self.home_team
 	  else
-	    cover_team = self.away_team
+	    if differential.abs > self.spread.abs
+	      cover_team = self.home_team
+	    else
+	      cover_team = self.away_team
+	    end
 	  end
 	end
+
+	puts "#{cover_team}"
+	self.bets.each do |bet|
+	  player = Player.find_by_id bet.player_id
+	  if bet.team == cover_team
+	    # Player wins money
+	    player.money += bet.wager
+	    player.bettable_money += 2 * bet.wager
+	  else
+	    # Player loses money
+	    player.money -= bet.wager
+	  end
+	  player.save!
+	end 
       end
-
-      puts "#{cover_team}"
-      self.bets.each do |bet|
-	player = Player.find_by_id bet.player_id
-	if bet.team == cover_team
-	  # Player wins money
-	  player.money += bet.wager
-	else
-	  # Player loses money
-	  player.money -= bet.wager
-	end
-	player.save!
-      end 
     end
 
 end

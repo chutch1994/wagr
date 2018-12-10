37707699f94f6f50d76448ba8bfb76710f72f8ea
Chris Hutchinson <chutchinson@covermymeds.com>
2018-12-10 16:53:23 -0500

    Improves UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
 	include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
   protected
 
   def configure_permitted_parameters
